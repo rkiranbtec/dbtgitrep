e16fadea1f06778eb9745219e7afb403c57f0de6
ravi kiran <ravikiran.btec@gmail.com>
2024-07-15 06:59:11 +0000

    commit

--- models/staging/DWH/order_lineitem.sql
+++ models/staging/DWH/order_lineitem.sql
@@ -4,7 +4,8 @@ select
     --line_key and order_key are both unique and non-null 
     --so we don't need a distinct count for unique_items
     count(1) as order_unique_items,
-    sum(line_quantity) as order_total_quantity
+    {{ calculate_sum('line_quantity') }} as order_total_quantity
+
 
 from {{ ref('stg_orders') }} as o 
 
